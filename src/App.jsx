72f5737e813fba940fedb294a9d2e0cd10c052b9
adriandabrowski1 <adrianda@hiof.no>
2026-03-15 02:21:54 +0100

    Oppsett

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>Movie Search</h1>
+        <p>Finn filmer med OMDb API</p>
+      </header>
+    </main>
+  )
+}
+
+export default App
